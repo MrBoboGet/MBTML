e6654aade12ba7931c4524e6d9171bf423a043a3
MrBoboGet <emanuelberggren01@gmail.com>
2025-12-29 00:02:04 +0100

    modal window

---
+++ TML/modal.lisp
@@ -0,0 +1,32 @@
+(import "TML.lisp")
+
+@tml
+<modal>
+    <absolute @window visible=false 
+    orientation=orientation? 
+    row-offset=row-offset?
+    col-offset=col-offset?
+    relative=relative?
+    width=width? 
+    height=height?>
+        <repl oneshot=true/>
+    </absolute>
+</modal>
+
+(defmethod set-window ((mod modal) base-window new-window callable)
+    (set-atr :window mod "visible" true)
+    (set-child :window mod new-window)
+    (push base-window new-window 
+        _(progn
+            (set-atr :window mod "visible" false)
+            (callable)
+        )
+   )
+)
+
+(defmethod set-window ((mod modal) base-window new-window)
+    (set-window mod base-window new-window _(progn null))
+)
+(defmethod window-active ((mod modal))
+    (set-window mod base-window new-window _(progn null))
+)
